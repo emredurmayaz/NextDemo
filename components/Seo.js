cd9fa181d96866e7326ccff1dc0c13a961e613c7
emredurmayaz <emredurmayaz@hotmail.com>
2022-08-18 15:33:04 +0300

    Gtm added to project

---
+++ components/Seo.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+function Seo() {
+  return (
+    <div>
+     <Head>
+        <title>Add a Shopping Cart to Any Website in Minutes - Snipcart</title>
+        <meta name="description" content="Add a shopping cart to your site in minutes. Works with any site builder, CMS, and framework. 20 000+ merchants trust our e-commerce solution for their website. Join them!"></meta>
+        <meta property="og:title" content="Add a Shopping Cart to Any Website in Minutes - Snipcart"></meta>
+        <meta property="og:description" content="Add a shopping cart to your site in minutes. Works with any site builder, CMS, and framework. 20 000+ merchants trust our e-commerce solution for their website. Join them!"></meta>
+        <meta property="og:url" content="https://snipcart.com/"></meta>
+        <meta property="og:type" content="website"></meta>
+      </Head>
+    </div>
+  );
+}
+
+export default Seo;
